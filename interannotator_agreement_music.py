5cd088aad48e8bb9e27e6ea21a6d6e0b8b26a663
timothydgreer <timothydgreer@gmail.com>
2020-01-17 15:45:34 -0800

    Add files via upload

--- interannotator_agreement_music.py
+++ interannotator_agreement_music.py
@@ -1,51 +1,49 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 27 15:37:27 2018
+Created on Fri Jan 17 14:34:52 2020
 
 @author: greert
 """
 
 import csv
-tim = []
-ben = []
-#different parms if karan is in
-#karan = []
+ann1 = []
+ann2 = []
+ann3 = []
 
 with open("raw_annotations_3_27.csv") as myFile:
     spamreader = csv.reader(myFile)
     for row in spamreader:
-        tim.append(int(row[0]))
-        ben.append(int(row[1]))
-        #karan.append(int(row[2]))
+        ann1.append(int(row[0]))
+        ann2.append(int(row[1]))
+        ann3.append(int(row[2]))
 
-pe = 1/3.0
+pe = 1/9.0
 po = 0
 count = 0
-for i in xrange(len(tim)):
-    if tim[i] == ben[i]:
+for i in range(len(ann1)):
+    if ann1[i] == ann2[i] and ann1[i] == ann3[i]:
         count = count+1
 po = count/1000.0
 kappa = (po-pe)/(1-pe)
 print(kappa)
-#.538 for tim and ben
 
 countw = 0
 #Weight by chance
-w = 8000/9.0
-for i in xrange(len(tim)):
-    if abs(tim[i] - ben[i]) == 2:
-        countw = countw+2
-    if abs(tim[i] - ben[i]) == 1:
-        countw = countw+1
-    else:
-        pass
+w = 24000/9.0
+for i in range(len(ann1)):
+    mysum = 0
+    mysum += abs(ann1[i] - ann2[i])
+    mysum += abs(ann2[i] - ann3[i])
+    mysum += abs(ann1[i] - ann3[i])
+    countw = countw+mysum
+    
 kappaw = 1.0 - (countw/w)
 print(kappaw)
-#.6445 for tim and ben
 
 
-#Possibilities for tim and ben: both on -1 1
+
+#Possibilities: both on -1 1
 #both on 0 1
 #both on 1 1
 #one on -1, one on 1 2
